63d4dae586f45de7d28490e821a0968f2d519c88
Malte <mphtokmh@gmail.com>
2023-03-12 12:56:19 +0100

    add datetime helper

---
+++ BlazorServerCalendarToInvoice/Utils/DateTimeHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorServerCalendarToInvoice.Utils
+{
+    public static class DateTimeHelper
+    {
+        public static DateTime GetStartOfMonth(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, 1, 0, 0, 0);
+        }
+        public static DateTime GetEndOfMonth(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month), 23, 59, 59);
+        }
+    }
+}
