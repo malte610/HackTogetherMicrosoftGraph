0b55578c129a7de768b1a85b9a7128a4f1befc52
Malte <mphtokmh@gmail.com>
2023-03-13 07:39:58 +0100

    add rdlc reporting service

---
+++ BlazorServerCalendarToInvoice/Rdlc/ReportService.cs
@@ -0,0 +1,45 @@
+﻿using BlazorServerCalendarToInvoice.Model;
+using System.Data;
+using System.Text;
+using BlazorServerCalendarToInvoice.Utils;
+using AspNetCore.Reporting;
+
+namespace BlazorServerCalendarToInvoice.Rdlc
+{
+    public class ReportService
+    {
+        public static byte[] CreateInvoice(InvoiceHeader invoice)
+        {
+            // prepare data
+
+            var tempList = new List<InvoiceHeader> { invoice };
+            DataTable dt_invoiceHeader = DataTableHelper.ToDataTable(tempList);
+            dt_invoiceHeader.Columns.RemoveAt(
+                dt_invoiceHeader.Columns.IndexOf("InvoiceLines"));
+            DataTable dt_invoiceLines = DataTableHelper.ToDataTable(invoice.InvoiceLines);
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Dictionary<string, string> parameters = new()
+            {
+                { "param", "Hi" }
+            };
+
+            // run report
+            string mimetype = "application/pdf";
+            int extension = 1;
+            string path;
+
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development)
+                path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "Rdlc", "Reports", "Invoice.rdlc"));
+            else
+                path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "wwwroot", "rdlc", "Invoice.rdlc"));
+
+            LocalReport localReport = new(path);
+            localReport.AddDataSource("dsInvoiceHeader", dt_invoiceHeader);
+            localReport.AddDataSource("dsInvoiceLines", dt_invoiceLines);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
+            return result.MainStream;
+        }
+    }
+}
---
+++ BlazorServerCalendarToInvoice/Utils/DataTableHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Reflection;
+
+namespace BlazorServerCalendarToInvoice.Utils
+{
+    public class DataTableHelper
+    {
+        internal static DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+#pragma warning disable CS8601 // Possible null reference assignment.
+                    values[i] = Props[i].GetValue(item, null);
+#pragma warning restore CS8601 // Possible null reference assignment.
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
+        }
+    }
+}
