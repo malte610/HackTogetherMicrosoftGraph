c5050c05d196d999bb73a099c580fd7d36776486
Malte <mphtokmh@gmail.com>
2023-03-14 10:57:55 +0100

    refactor CreateFolderAndFiles to depended BatchRequest

---
+++ BlazorServerCalendarToInvoice/Model/MyDriveItemFile.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Graph;
+using System.Text.Json.Serialization;
+
+namespace BlazorServerCalendarToInvoice.Model
+{
+    /// <summary>
+    /// Same story as MyDriveItemFolder.
+    /// </summary>
+    public class MyDriveItemFile
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("file")]
+        public Microsoft.Graph.File? File { get; set; }
+    }
+}
---
+++ BlazorServerCalendarToInvoice/Model/MyDriveItemFolder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Graph;
+using System.Text.Json.Serialization;
+
+namespace BlazorServerCalendarToInvoice.Model
+{
+    /// <summary>
+    /// This class is a substitute for DriveItem. As soon as I used DriveItem in a BatchRequest to create a folder
+    /// it complained with BadRequest that I should set 'workbook' later. I didn't set workbook. So I made this class.
+    /// </summary>
+    public class MyDriveItemFolder
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("folder")]
+        public Folder? Folder { get; set; }
+    }
+}
